28b5e49e1261c31129a85427d1b08faec23f6604
Ralf Tambets <ralf.tambets@gmail.com>
2020-10-19 16:04:40 +0300

    completed task 2

--- res/js/script.js
+++ res/js/script.js
@@ -1,5 +1,3 @@
-let activeUserInfo = null;
-
 $(function () {
 
     $('#avatar-button').click(function () {
@@ -14,6 +12,24 @@ $(function () {
         alert('Error loading user info')
       });
 
+    loadPosts()
+      .then(function (posts) {
+        displayPosts(posts)
+        let likeButtons = $('.like-button');
+        for (let likeButton of likeButtons) {
+          $(likeButton).click(function (){
+            if ($(this).hasClass('liked')){
+              $(this).removeClass('liked');
+            }
+            else {
+              $(this).addClass('liked');
+            }
+          })
+        }
+      })
+      .catch(function() {
+        alert('Error loading posts info')
+      });
 });
 
 function loadActiveUserInfo() {
@@ -30,8 +46,66 @@ function loadActiveUserInfo() {
   )
 }
 
+function loadPosts() {
+  return $.get(
+    {
+      url: 'https://private-anon-ffc569fdf1-wad20postit.apiary-mock.com/posts',
+      success: function(response){
+        return response;
+      },
+      error: function(){
+        alert('error')
+      }
+    }
+  )
+}
+
 function displayActiveUserInfo(user) {
   $('#active-user-name').text(user.firstname + " " + user.lastname);
   $('#active-user-email').text(user.email);
   $('#avatar-image').attr('src', user.avatar);
+}
+
+function displayPosts(posts) {
+  for (let post of posts){
+    result = "";
+    result += `<div class="post">
+      <div class="post-author">
+        <span class="post-author-info">
+          <img src="${post.author.avatar}" alt="Post author">
+          <small>${post.author.firstname + " " + post.author.lastname}</small>
+        </span>
+        <small>${post.createTime}</small>
+      </div>`
+
+    if (post.media !== null){
+      if (post.media.type === "image"){
+        result += `<div class="post-image">
+          <img src="${post.media.url}" alt="">
+        </div>`
+      }
+      else if (post.media.type === "video"){
+        result += `<div class="post-video">
+          <video width="320" height="240" controls>
+            <source src=${post.media.url} type="video/mp4">
+            <source src=${post.media.url} type="video/ogg">
+            Your browser does not support the video tag.
+            </video>
+        </div>`
+      }
+    }
+
+    if (post.text !== null){
+      result += `<div class="post-title">
+        <h3>${post.text}</h3>
+      </div>`
+    }
+
+    result += `<div class="post-actions">
+        <button type="button" name="like" class="like-button">${post.likes}</button>
+      </div>
+    </div>`
+
+    $('.main-container').append(result);
+  }
 }
