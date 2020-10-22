013e83162ba8b5f520569d538d73dd0665d7d4c6
kristiinakulbok <kristiina.kulbok@gmail.com>
2020-10-22 14:50:35 +0300

    drop-down should look nice now; moved the like button on videos to the right place; added header to browse.html and functions for retrieving profiles

--- res/js/script.js
+++ res/js/script.js
@@ -1,75 +1,97 @@
 $(function () {
 
     $('#avatar-button').click(function () {
-      $('#active-user-info-container').toggle();
+        $('#active-user-info-container').toggle();
     });
 
     loadActiveUserInfo()
-      .then(function (user) {
-        displayActiveUserInfo(user)
-      })
-      .catch(function() {
-        alert('Error loading user info')
-      });
+        .then(function (user) {
+            displayActiveUserInfo(user)
+        })
+        .catch(function () {
+            alert('Error loading user info')
+        });
 
     loadPosts()
-      .then(function (posts) {
-        displayPosts(posts)
-        let likeButtons = $('.like-button');
-        for (let likeButton of likeButtons) {
-          $(likeButton).click(function (){
-            if ($(this).hasClass('liked')){
-              $(this).removeClass('liked');
+        .then(function (posts) {
+            displayPosts(posts)
+            let likeButtons = $('.like-button');
+            for (let likeButton of likeButtons) {
+                $(likeButton).click(function () {
+                    if ($(this).hasClass('liked')) {
+                        $(this).removeClass('liked');
+                    } else {
+                        $(this).addClass('liked');
+                    }
+                })
             }
-            else {
-              $(this).addClass('liked');
-            }
-          })
-        }
-      })
-      .catch(function() {
-        alert('Error loading posts info')
-      });
+        })
+        .catch(function () {
+            alert('Error loading posts info')
+        });
+
+    loadProfiles()
+        .then(function (profiles) {
+            displayProfiles(profiles)
+        })
+        .catch(function () {
+            alert('Error loading profile info')
+        });
+
 });
 
 function loadActiveUserInfo() {
-  return $.get(
-    {
-      url: 'https://private-anon-ffc569fdf1-wad20postit.apiary-mock.com/users/1',
-      success: function(response){
-        return response;
-      },
-      error: function(){
-        alert('error')
-      }
-    }
-  )
+    return $.get(
+        {
+            url: 'https://private-anon-ffc569fdf1-wad20postit.apiary-mock.com/users/1',
+            success: function (response) {
+                return response;
+            },
+            error: function () {
+                alert('error')
+            }
+        }
+    )
 }
 
 function loadPosts() {
-  return $.get(
-    {
-      url: 'https://private-anon-ffc569fdf1-wad20postit.apiary-mock.com/posts',
-      success: function(response){
-        return response;
-      },
-      error: function(){
-        alert('error')
-      }
-    }
-  )
+    return $.get(
+        {
+            url: 'https://private-anon-ffc569fdf1-wad20postit.apiary-mock.com/posts',
+            success: function (response) {
+                return response;
+            },
+            error: function () {
+                alert('error')
+            }
+        }
+    )
+}
+
+function loadProfiles() {
+    return $.get(
+        {
+            url: 'https://private-anon-f1f6efae53-wad20postit.apiary-mock.com/profiles',
+            success: function (response) {
+                return response;
+            },
+            error: function () {
+                alert('error')
+            }
+        }
+    )
 }
 
 function displayActiveUserInfo(user) {
-  $('#active-user-name').text(user.firstname + " " + user.lastname);
-  $('#active-user-email').text(user.email);
-  $('#avatar-image').attr('src', user.avatar);
+    $('#active-user-name').text(user.firstname + " " + user.lastname);
+    $('#active-user-email').text(user.email);
+    $('#avatar-image').attr('src', user.avatar);
 }
 
 function displayPosts(posts) {
-  for (let post of posts){
-    result = "";
-    result += `<div class="post">
+    for (let post of posts) {
+        result = "";
+        result += `<div class="post">
       <div class="post-author">
         <span class="post-author-info">
           <img src="${post.author.avatar}" alt="Post author">
@@ -78,34 +100,47 @@ function displayPosts(posts) {
         <small>${post.createTime}</small>
       </div>`
 
-    if (post.media !== null){
-      if (post.media.type === "image"){
-        result += `<div class="post-image">
+        if (post.media !== null) {
+            if (post.media.type === "image") {
+                result += `<div class="post-image">
           <img src="${post.media.url}" alt="">
         </div>`
-      }
-      else if (post.media.type === "video"){
-        result += `<div class="post-video">
+            } else if (post.media.type === "video") {
+                result += `<div class="post-video">
           <video width="320" height="240" controls>
             <source src=${post.media.url} type="video/mp4">
             <source src=${post.media.url} type="video/ogg">
             Your browser does not support the video tag.
             </video>
         </div>`
-      }
-    }
+            }
+        }
 
-    if (post.text !== null){
-      result += `<div class="post-title">
+        if (post.text !== null) {
+            result += `<div class="post-title">
         <h3>${post.text}</h3>
       </div>`
-    }
+        }
 
-    result += `<div class="post-actions">
+        result += `<div class="post-actions">
         <button type="button" name="like" class="like-button">${post.likes}</button>
       </div>
     </div>`
 
-    $('.main-container').append(result);
-  }
+        $('.main-container').append(result);
+    }
+}
+
+function displayProfiles(profiles) {
+    result = "";
+    for (let profile of profiles) {
+        result += `<div class="profile">
+        <span class="profile-info">
+          <img src="${profile.avatar}" alt="Profile picture">
+          <small>${profile.firstname + " " + profile.lastname}</small>
+        </span>
+      </div>`
+    }
+
+    $('.profile-container').append(result);
 }
